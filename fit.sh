aa3ff56679fcef4d023e4900518266513b979a37
Dan Vanderkam <danvdk@gmail.com>
2026-06-02 15:07:23 -0400

    tweaks for brooklyn multivolume

--- fit.sh
+++ fit.sh
@@ -7,8 +7,10 @@ set -x
 dir=$1
 tag=$2
 
-uv run mapsnap/georef_from_labels.py $dir/*.scaled.jpg \
-    --centerlines $dir/centerlines.geojson \
+centerlines=$dir/../centerlines.geojson
+
+uv run mapsnap/georef_from_labels.py $dir/p*.raw.jpg \
+    --centerlines $centerlines \
     --min-long-side 45 \
     --min-short-side 20 \
     --min-confidence 0.15 \
@@ -18,21 +20,15 @@ uv run mapsnap/georef_from_labels.py $dir/*.scaled.jpg \
 
 if [ -e $dir/main.iiif.json ]; then
     ref_iiif=$dir/main.iiif.json
-else
+elif [ -e $dir/loc.iiif.json ]; then
     ref_iiif=$dir/loc.iiif.json
-fi
-
-# --edge-margin 0
-
-if [ -e $dir/main.iiif.json ]; then
-    ref_iiif=$dir/main.iiif.json
 else
-    ref_iiif=$dir/loc.iiif.json
+    ref_iiif=$dir/*.manifest.json
 fi
 
 uv run python mapsnap/make_iiif_georef.py \
     $ref_iiif $dir'/*.georef.json' \
-    --centerlines $dir/centerlines.geojson \
+    --centerlines $centerlines \
     --output $dir/$tag.iiif.json
 
 if [ -e $dir/main.iiif.json ]; then
